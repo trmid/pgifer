836117cf48a0aa558f38384cbcd5518dba943286
trmid <trmidpoint@gmail.com>
2023-01-13 11:04:23 -0500

    build

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const cacheName = `0.1.4`; // Change value to force update
-const filesToCache = `["./","build/bundle.js","build/bundle.css","favicon.png","gif.js","gif.js.map","gif.worker.js","gif.worker.js.map","icofont/fonts/icofont.woff","icofont/fonts/icofont.woff2","icofont/icofont.min.css","index.html"]`; // Generated filenames as string array
+const cacheName = `0.1.5`; // Change value to force update
+const filesToCache = `["./","build/bundle.js","build/bundle.css","favicon.png","gif.js","gif.js.map","gif.worker.js","gif.worker.js.map","icofont/fonts/icofont.woff","icofont/fonts/icofont.woff2","icofont/icofont.min.css","index.html","small-example.gif"]`; // Generated filenames as string array
 
 self.addEventListener("install", event => {
   
