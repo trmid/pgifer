21d7d927eb03c14d2bef0926efa9baaecbad33c1
trmid <trmidpoint@gmail.com>
2023-01-13 11:03:19 -0500

    change .link to .limo

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 
 <hr>
 <p>
-  Made by <strong><a href="https://trmid.eth.link" target="_blank" rel="noreferrer">trmid.eth</a></strong>.
+  Made by <strong><a href="https://trmid.eth.limo" target="_blank" rel="noreferrer">trmid.eth</a></strong>.
 </p>
